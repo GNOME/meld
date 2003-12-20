e80293ead226872d888b4e8712520eb6ae4aaefd
Stephen Kennedy <stevek@src.gnome.org>
2003-12-20 18:49:06 +0000

    fix typos.

--- po/update-pot.sh
+++ po/update-pot.sh
@@ -2,5 +2,5 @@
 # generate meld.pot
 intltool-update -g meld -p
 # remove garbage comments
-cp meld.pot meld.pot.in && cat meld.pot.in | sed -e "/^#\..*/d" > meld.pot
-rm meld.pot.in
+#cp meld.pot meld.pot.in && cat meld.pot.in | sed -e "/^#\..*/d" > meld.pot
+#rm meld.pot.in
