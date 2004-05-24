a63b159a60a529c29587f0d50d2a590ce44f1f63
Stephen Kennedy <stevek@src.gnome.org>
2004-05-24 17:40:33 +0000

    Use gtk.main instead of deprecated gtk.mainloop

--- gnomeglade.py
+++ gnomeglade.py
@@ -105,7 +105,7 @@ class GtkApp(Base):
     def mainloop(self):
         """Enter the gtk main loop.
         """
-        gtk.mainloop()
+        gtk.main()
 
     def quit(*args):
         """Signal the gtk main loop to quit.
