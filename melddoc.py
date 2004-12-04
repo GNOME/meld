08456c79aaa575a2aec5f0a316eab2ebc426e626
Stephen Kennedy <stevek@src.gnome.org>
2004-12-04 11:24:12 +0000

    Delete old code.

--- melddoc.py
+++ melddoc.py
@@ -16,7 +16,6 @@
 
 import gobject
 import task
-import undo
 import gtk
 import os
 
@@ -31,21 +30,16 @@ class MeldDoc(gobject.GObject):
         'label-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_STRING,)),
         'file-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_STRING,)),
         'create-diff': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,)),
-        'status-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,))
+        'status-changed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, (gobject.TYPE_PYOBJECT,)),
+        'closed': (gobject.SIGNAL_RUN_FIRST, gobject.TYPE_NONE, ()),
     }
 
     def __init__(self, prefs):
         self.__gobject_init__()
-        self.undosequence = undo.UndoSequence()
-        self.undosequence_busy = 0
         self.scheduler = task.FifoScheduler()
         self.prefs = prefs
         self.prefs.notify_add(self.on_preference_changed)
-        self.num_panes = 0
         self.label_text = _("untitled")
-
-    def save(self):
-        pass
 
     def stop(self):
         if len(self.scheduler.tasks):
@@ -63,51 +57,12 @@ class MeldDoc(gobject.GObject):
                 cmd = self.prefs.get_custom_editor_command(files)
                 os.spawnvp(os.P_NOWAIT, cmd[0], cmd)
 
-    def action__undo(self):
-        if self.undosequence.can_undo():
-            self.undosequence_busy = 1
-            try:
-                self.undosequence.undo()
-            finally:
-                self.undosequence_busy = 0
-
-    def action__redo(self):
-        if self.undosequence.can_redo():
-            self.undosequence_busy = 1
-            try:
-                self.undosequence.redo()
-            finally:
-                self.undosequence_busy = 0
-            self.undosequence_busy = 0
-
-    def on_find_activate(self, *extra):
-        pass
-    def on_find_next_activate(self, *extra):
-        pass
-    def on_replace_activate(self, *extra):
-        pass
-
-    def on_copy_activate(self, *args):
-        pass
-    def on_cut_activate(self, *args):
-        pass
-    def on_paste_activate(self, *args):
-        pass
-
     def on_preference_changed(self, key, value):
         pass
 
-    def on_file_changed(self, filename):
-        pass
-
-    def label_changed(self):
-        self.emit("label-changed", self.label_text)
-
-    def on_switch_event(self):
-        pass
-
-    def on_delete_event(self, appquit=0):
+    def on_container_delete_event(self, app_quit=0):
         """Called when the docs container is about to close.
+
            A doc normally returns gtk.RESPONSE_OK but may return
            gtk.RESPONSE_CANCEL which requests the container
            to not delete it. In the special case when the
@@ -117,10 +72,20 @@ class MeldDoc(gobject.GObject):
         """
         return gtk.RESPONSE_OK
 
-    def on_quit_event(self):
-        """Called when the docs container is about to close.
-           There is no way to interrupt the quit event.
+    def on_container_quit_event(self):
+        """Called when the container app is closing.
+
+           The doc should clean up resources, but not block.
         """
         pass
 
+    def on_container_file_changed(self, fname):
+        """Called when the container app has modified a file.
+        """
+        pass
+
+    def on_container_switch_event(self):
+        """Called when the container app switches to this tab.
+        """
+        pass
 gobject.type_register(MeldDoc)
