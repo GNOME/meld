2a752bd1bf67cdfea25a07c425c9e15c120f32eb
Stephen Kennedy <stevek@src.gnome.org>
2006-03-30 20:21:53 +0000

    Update copyright date.

--- diffutil.py
+++ diffutil.py
@@ -1,4 +1,4 @@
-### Copyright (C) 2002-2005 Stephen Kennedy <stevek@gnome.org>
+### Copyright (C) 2002-2006 Stephen Kennedy <stevek@gnome.org>
 
 ### This program is free software; you can redistribute it and/or modify
 ### it under the terms of the GNU General Public License as published by
