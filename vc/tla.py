9f69c8a7c379714db9fd3ce0d7ebf4be1faf0256
Stephen Kennedy <stevek@src.gnome.org>
2007-12-06 22:41:28 +0000

    Add <> to email
    svn path=/trunk/; revision=964

--- vc/tla.py
+++ vc/tla.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*- 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
-#Copyright (c) 2005 Ali Afshar aafshar@gmail.com
+#Copyright (c) 2005 Ali Afshar <aafshar@gmail.com>
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
