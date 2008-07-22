89c8d77a818a0f84518e400a83d5dea191d93baa
Stephen Kennedy <stevek@src.gnome.org>
2008-07-22 07:52:33 +0000

    Handle spaces in svn path Bug 522864
    svn path=/trunk/; revision=1028

--- vc/svn.py
+++ vc/svn.py
@@ -54,7 +54,7 @@ class Vc(_vc.Vc):
 
         while 1:
             try:
-                entries = os.popen("svn status -Nv "+directory).read()
+                entries = os.popen('svn status -Nv "%s"'%directory).read()
                 break
             except OSError, e:
                 if e.errno != errno.EAGAIN:
