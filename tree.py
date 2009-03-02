GNOME/meld

commit 19e5b4b0caa1053ec4efb4128da2f13a01b92221

Use equivalent parent class constructor code
svn path=/trunk/; revision=1196

vcview.py
@@ -89,11 +89,7 @@ COL_LOCATION, COL_STATUS, COL_REVISION, COL_TAG, COL_OPTIONS, COL_END = range(tr
 
 class VcTreeStore(tree.DiffTreeStore):
     def __init__(self):
-        types = [type("")] * COL_END
-        types[tree.COL_ICON] = type(tree.pixbuf_file)
-        gtk.TreeStore.__init__(self, *types)
-        self.ntree = 1
-        self._setup_default_styles()
+        tree.DiffTreeStore.__init__(self, 1)
         self.textstyle[tree.STATE_MISSING] = '<span foreground="#000088" strikethrough="true" weight="bold">%s</span>'
 
 ################################################################################

commit 7b744f6fe1931ba5e43788c5a95b37f394f90b04

Fix bug introduced in r1196 : COL_NUM should be passed to constructor, else it will be wrong
svn path=/trunk/; revision=1201

tree.py
@@ -36,8 +36,8 @@ pixbuf_file_new = load("tree-file-new.png")
 pixbuf_file_changed = load("tree-file-changed.png")
 
 class DiffTreeStore(gtk.TreeStore):
-    def __init__(self, ntree = 3):
-        types = [type("")] * COL_END * ntree
+    def __init__(self, ntree = 3, num_col = COL_END):
+        types = [type("")] * num_col * ntree
         types[COL_ICON*ntree:COL_ICON*ntree+ntree] = [type(pixbuf_file)] * ntree
         gtk.TreeStore.__init__(self, *types)
         self.ntree = ntree

vcview.py
@@ -89,7 +89,7 @@ COL_LOCATION, COL_STATUS, COL_REVISION, COL_TAG, COL_OPTIONS, COL_END = range(tr
 
 class VcTreeStore(tree.DiffTreeStore):
     def __init__(self):
-        tree.DiffTreeStore.__init__(self, 1)
+        tree.DiffTreeStore.__init__(self, 1, COL_END)
         self.textstyle[tree.STATE_MISSING] = '<span foreground="#000088" strikethrough="true" weight="bold">%s</span>'
 
 ################################################################################