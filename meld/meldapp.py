0b41ffa34a1edaba488a1f57cc4c56fef123980b
Kai Willadsen <kai.willadsen@gmail.com>
2010-12-24 08:25:56 +1000

    Fix display of translator credits in About dialogue

--- meld/meldapp.py
+++ meld/meldapp.py
@@ -448,6 +448,8 @@ class MeldApp(gnomeglade.Component):
     def on_menu_about_activate(self, *extra):
         gtk.about_dialog_set_url_hook(lambda dialog, uri: misc.open_uri(uri))
         builder = gtk.Builder()
+        # FIXME: domain literal duplicated from bin/meld
+        builder.set_translation_domain("meld")
         builder.add_objects_from_file(paths.ui_dir("meldapp.ui"), ["about"])
         about = builder.get_object("about")
         about.props.version = version
