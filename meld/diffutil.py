GNOME/meld

commit 92295b5f64082709c01ec799b06c348b2ecb2d77

Introduce namedtuple chunk representation to improve clarity
This commit also adapts a couple of existing chunk manipulation methods
to correctly return namedtuple-style chunks.

meld/diffutil.py
@@ -19,7 +19,7 @@ import difflib
 
 import gobject
 
-from matchers import MyersSequenceMatcher
+from matchers import DiffChunk, MyersSequenceMatcher
 
 ################################################################################
 #
@@ -62,7 +62,8 @@ opcode_reverse = {
 }
 
 def reverse_chunk(chunk):
-    return opcode_reverse[chunk[0]], chunk[3], chunk[4], chunk[1], chunk[2]
+    tag = opcode_reverse[chunk[0]]
+    return DiffChunk._make((tag, chunk[3], chunk[4], chunk[1], chunk[2]))
 
 ################################################################################
 #
@@ -174,16 +175,17 @@ class Differ(gobject.GObject):
     def _consume_blank_lines(self, c, texts, pane1, pane2):
         if c is None:
             return None
-        c0 = c[0]
+
+        tag = c.tag
         c1, c2 = self._find_blank_lines(texts[pane1], c[1], c[2])
         c3, c4 = self._find_blank_lines(texts[pane2], c[3], c[4])
         if c1 == c2 and c3 == c4:
             return None
-        if c1 == c2 and c[0] == "replace":
+        if c1 == c2 and tag == "replace":
             c0 = "insert"
-        elif c3 == c4 and c[0] == "replace":
+        elif c3 == c4 and tag == "replace":
             c0 = "delete"
-        return (c0, c1, c2, c3, c4)
+        return DiffChunk._make((tag, c1, c2, c3, c4))
 
     def _find_blank_lines(self, txt, lo, hi):
         for line in range(lo, hi):

meld/matchers.py
@@ -14,6 +14,7 @@
 ### along with this program; if not, write to the Free Software
 ### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from collections import namedtuple
 import difflib
 
 
@@ -51,6 +52,9 @@ def find_common_suffix(a, b):
     return 0
 
 
+DiffChunk = namedtuple('DiffChunk', 'tag, start_a, end_a, start_b, end_b')
+
+
 class MyersSequenceMatcher(difflib.SequenceMatcher):
 
     def __init__(self, isjunk=None, a="", b=""):
@@ -71,8 +75,12 @@ class MyersSequenceMatcher(difflib.SequenceMatcher):
                 pass
         return self.matching_blocks
 
+    def get_opcodes(self):
+        opcodes = difflib.SequenceMatcher.get_opcodes(self)
+        return [DiffChunk._make(chunk) for chunk in opcodes]
+
     def get_difference_opcodes(self):
-        return filter(lambda x: x[0] != "equal", self.get_opcodes())
+        return [chunk for chunk in self.get_opcodes() if chunk.tag != "equal"]
 
     def preprocess_remove_prefix_suffix(self, a, b):
         # remove common prefix and common suffix

commit 9ca55149e22347b1fd83d581b096f23939bed83b

Fix regression in three-way diffs and on-the-fly updates
This commit fixes a regression in commit 92295b5 that caused three-way
diffs and on-the-fly updating to fail because they were synthesizing
new chunks, but not using the namedtuple representation.

Included in this fix is a small rework of some of the code in
Differ._change_sequence, being more explicit about what the various
loops are actually doing.

meld/diffutil.py
@@ -238,13 +238,18 @@ class Differ(gobject.GObject):
         linesx = texts[x][rangex[0]:rangex[1]]
         lines1 = texts[1][range1[0]:range1[1]]
         #print "<<<\n%s\n===\n%s\n>>>" % ("\n".join(linesx),"\n".join(lines1))
+
+        def offset(c, o1, o2):
+            return DiffChunk._make((c[0], c[1] + o1, c[2] + o1,
+                                          c[3] + o2, c[4] + o2))
+
         newdiffs = self._matcher(None, lines1, linesx).get_difference_opcodes()
-        newdiffs = [ (c[0], c[1]+range1[0],c[2]+range1[0], c[3]+rangex[0],c[4]+rangex[0]) for c in newdiffs]
+        newdiffs = [offset(c, range1[0], rangex[0]) for c in newdiffs]
+
         if hiidx < len(self.diffs[which]):
-            self.diffs[which][hiidx:] = [ (c[0],
-                                           c[1] + lines_added[1], c[2] + lines_added[1],
-                                           c[3] + lines_added[x], c[4] + lines_added[x])
-                                                for c in self.diffs[which][hiidx:] ]
+            offset_diffs = [offset(c, lines_added[1], lines_added[x]) for c
+                                                  in self.diffs[which][hiidx:]]
+            self.diffs[which][hiidx:] = offset_diffs
         self.diffs[which][loidx:hiidx] = newdiffs
 
     def _range_from_lines(self, textindex, lines):
@@ -336,8 +341,8 @@ class Differ(gobject.GObject):
                 tag = "insert"
         else:
             tag = "conflict"
-        out0 = (tag, l1, h1, l0, h0)
-        out1 = (tag, l1, h1, l2, h2)
+        out0 = DiffChunk._make((tag, l1, h1, l0, h0))
+        out1 = DiffChunk._make((tag, l1, h1, l2, h2))
         yield out0, out1
 
     def _merge_diffs(self, seq0, seq1, texts):