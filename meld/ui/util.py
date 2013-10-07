bee6b96d99e3fea7fe007a73999e7ae15a517026
Kai Willadsen <kai.willadsen@gmail.com>
2013-10-07 11:26:37 +1000

    ui.util: Add new file for minimal gnomeglade-like help

---
+++ meld/ui/util.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2002-2008 Stephen Kennedy <stevek@gnome.org>
+# Copyright (C) 2010, 2013 Kai Willadsen <kai.willadsen@gmail.com>
+
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import os
+
+from gi.repository import Gtk
+
+import meld.conf
+
+# Import support module to get all builder-constructed widgets in the namespace
+from meld.ui import gladesupport
+
+
+def ui_file(filename):
+    return os.path.join(meld.conf.DATADIR, "ui", filename)
+
+
+def get_widget(filename, widget):
+    builder = Gtk.Builder()
+    builder.set_translation_domain(meld.conf.__package__)
+    builder.add_objects_from_file(ui_file(filename), [widget])
+    return builder.get_object(widget)
+
+
+def get_builder(filename):
+    builder = Gtk.Builder()
+    builder.set_translation_domain(meld.conf.__package__)
+    builder.add_from_file(ui_file(filename))
+    return builder
