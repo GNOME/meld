90ee363ea99dcac4ded3d4a82f141fe75ce6d955
Kai Willadsen <kai.willadsen@gmail.com>
2013-11-07 06:19:44 +1000

    build_helpers: Add a copyright header
    The original source doesn't actually use copyright headers anywhere;
    the header here is based on the LICENSE file and the Debian metadata.
    Authors list is guessed. The Canonical copyright appears on several
    related files, but without any current dates.

--- meld/build_helpers.py
+++ meld/build_helpers.py
@@ -1,5 +1,25 @@
-# Created by Sebastian Heinlein
-# Modified by Kai Willadsen
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+# USA.
+
+# Copied and adapted from the DistUtilsExtra project
+# Created by Sebastian Heinlein and Martin Pitt
+# Copyright Canonical Ltd.
+
+# Modified by Kai Willadsen for the Meld project
+# Copyright (C) 2013 Kai Willadsen <kai.willadsen@gmail.com>
+
 
 import distutils.cmd
 import distutils.command.build
