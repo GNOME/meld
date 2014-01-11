259a0955b94df2bcbd81716a44cc1b564d3076ff
Kai Willadsen <kai.willadsen@gmail.com>
2014-01-11 10:12:12 +1000

    Update license headers to current recommended GPL text

--- meld/ui/msgarea.py
+++ meld/ui/msgarea.py
@@ -2,23 +2,21 @@
 #
 # Copyright (C) 2007,2008 Colin Walters <walters@verbum.org>
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, either version 2 of the License, or (at
+# your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-# USA.
-
-# Additional modifications made for use in Meld and adaptations for newer
-# GTK+.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional modifications made for use in Meld and adaptations for
+# newer GTK+.
 # Copyright (C) 2013 Kai Willadsen <kai.willadsen@gmail.com>
 
 from gi.repository import Gtk
