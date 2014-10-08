411f5a031b026f84f32da1bc164da9f7fc3705d3
Mark Pariente <markpariente@gmail.com>
2014-10-08 10:43:29 -0700

    Move property handling to settings.py
    As requested by Kai, removed style-scheme property handling from
    filediff.py and moved it to meld.settings. Now meld.buffer just listens
    to the changed signal of meld.settings and just changes scheme as a
    response. Also moved scheme lookup to meld.settings.

--- meld/settings.py
+++ meld/settings.py
@@ -19,6 +19,7 @@ from gi.repository import Gio
 from gi.repository import GLib
 from gi.repository import GObject
 from gi.repository import Pango
+from gi.repository import GtkSource
 
 import meld.conf
 import meld.filters
@@ -41,6 +42,7 @@ class MeldSettings(GObject.GObject):
         self.on_setting_changed(settings, 'filename-filters')
         self.on_setting_changed(settings, 'text-filters')
         self.on_setting_changed(settings, 'use-system-font')
+        self.style_scheme = self._style_scheme_from_gsettings()
         settings.connect('changed', self.on_setting_changed)
 
     def on_setting_changed(self, settings, key):
@@ -56,8 +58,13 @@ class MeldSettings(GObject.GObject):
             self.font = self._current_font_from_gsetting()
             self.emit('changed', 'font')
         elif key in ('style-scheme'):
-            self.style_scheme = settings.get_string('style-scheme')
+            self.style_scheme = self._style_scheme_from_gsettings()
             self.emit('changed', 'style-scheme')
+
+    def _style_scheme_from_gsettings(self):
+        manager = GtkSource.StyleSchemeManager.get_default()
+        return GtkSource.StyleSchemeManager.get_scheme(manager,
+                settings.get_string('style-scheme'))
 
     def _filters_from_gsetting(self, key, filt_type):
         filter_params = settings.get_value(key)
