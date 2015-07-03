456dc9776ac4df94788e04388c23e0eafd6c7948
Kai Willadsen <kai.willadsen@gmail.com>
2015-07-04 07:27:28 +1000

    linkmap: Stop rendering frame that doesn't work
    This doesn't currently render anything, and will never do what we
    want anyway because of how LinkMap and TextViews line up.

--- meld/linkmap.py
+++ meld/linkmap.py
@@ -54,15 +54,12 @@ class LinkMap(Gtk.DrawingArea):
 
         context.set_line_width(1.0)
         allocation = self.get_allocation()
-        style = self.get_style_context()
 
         pix_start = [t.get_visible_rect().y for t in self.views]
         y_offset = [t.translate_coordinates(self, 0, 0)[1] + 1 for t in self.views]
 
         clip_y = min(y_offset) - 1
         clip_height = max(t.get_visible_rect().height for t in self.views) + 2
-        Gtk.render_frame(
-            style, context, 0, clip_y, allocation.width, clip_height)
         context.rectangle(0, clip_y, allocation.width, clip_height)
         context.clip()
 
