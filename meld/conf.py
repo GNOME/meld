55265dd799508910e98641a3f06a9cca2ce379b3
Kai Willadsen <kai.willadsen@gmail.com>
2015-07-23 07:12:08 +1000

    conf: Bump for new stable release

--- meld/conf.py
+++ meld/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 __package__ = "meld"
-__version__ = "3.13.4"
+__version__ = "3.14.0"
 
 # START; these paths are clobbered on install by meld.build_helpers
 DATADIR = os.path.join(sys.prefix, "share", "meld")
