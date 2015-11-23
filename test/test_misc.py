33b7e01ae00070ec21d205a5d922f8bd52a15c9e
David Rabel <David.Rabel@NoreSoft.com>
2015-11-23 20:36:33 +0100

    New test intervals for helper for merging overlapping ranges

--- test/test_misc.py
+++ test/test_misc.py
@@ -9,9 +9,13 @@ from meld.misc import merge_intervals
     # No overlap
     ([(1, 5), (6, 9), (10, 11)], [(1, 5), (6, 9), (10, 11)]),
     # Two overlap points between ranges
-    ([(1, 5), (5, 9), (10, 11), (11, 20)], [(1, 9), (10, 20)]),
+    ([(1, 5), (5, 9), (10, 12), (11, 20)], [(1, 9), (10, 20)]),
     # Two overlap points between ranges, out of order
-    ([(5, 9), (1, 5), (11, 20), (10, 11)], [(1, 9), (10, 20)]),
+    ([(5, 9), (1, 5), (11, 20), (10, 12)], [(1, 9), (10, 20)]),
+    # Two equal ranges
+    ([(1, 5), (7, 8), (1, 5)], [(1, 5), (7, 8)]),
+    # Three ranges overlap
+    ([(1, 5), (4, 10), (9, 15)], [(1, 15)])
 ])
 def test_merge_intervals(intervals, expected):
     merged = merge_intervals(intervals)
