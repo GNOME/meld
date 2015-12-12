4ce83446fb68c679e0d2803d3af4e3c7e5854cd4
Kai Willadsen <kai.willadsen@gmail.com>
2015-12-13 08:04:41 +1000

    diffmap: Update copyright years

--- meld/diffmap.py
+++ meld/diffmap.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2002-2009 Stephen Kennedy <stevek@gnome.org>
-# Copyright (C) 2009-2013 Kai Willadsen <kai.willadsen@gmail.com>
+# Copyright (C) 2009-2013, 2015 Kai Willadsen <kai.willadsen@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
