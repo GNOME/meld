792a24a28533c3f80bd122c8209008de88dcd707
Kai Willadsen <kai.willadsen@gmail.com>
2016-01-31 06:45:11 +1000

    ui.notebooklabel: Remove now-unnecessary CSS that's causing warnings

--- meld/ui/notebooklabel.py
+++ meld/ui/notebooklabel.py
@@ -25,16 +25,6 @@ from meld.conf import _
 class NotebookLabel(Gtk.HBox):
     __gtype_name__ = "NotebookLabel"
 
-    css = """
-    * {
-        -GtkButton-default-border : 0;
-        -GtkButton-default-outside-border : 0;
-        -GtkButton-inner-border: 0;
-        -GtkWidget-focus-line-width : 0;
-        -GtkWidget-focus-padding : 0;
-        padding : 0;
-    }
-    """
     tab_width_in_chars = 30
 
     def __init__(self, iconname, text, onclose):
@@ -72,13 +62,7 @@ class NotebookLabel(Gtk.HBox):
         image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.MENU)
         image.set_tooltip_text(_("Close tab"))
         button.add(image)
-        button.set_name("meld-tab-close-button")
         button.connect("clicked", onclose)
-
-        context = button.get_style_context()
-        provider = Gtk.CssProvider()
-        provider.load_from_data(self.css)
-        context.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
         icon = Gtk.Image.new_from_icon_name(iconname, Gtk.IconSize.MENU)
 
