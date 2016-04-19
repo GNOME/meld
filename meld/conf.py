f7b4f47cc55720d86394b25bc7b25c323ccacbec
Kai Willadsen <kai.willadsen@gmail.com>
2016-04-20 05:48:41 +1000

    Post-release version bump

--- meld/conf.py
+++ meld/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 __package__ = "meld"
-__version__ = "3.15.3"
+__version__ = "3.15.4"
 
 # START; these paths are clobbered on install by meld.build_helpers
 DATADIR = os.path.join(sys.prefix, "share", "meld")
