457c8753158b43f49bdf0a3bde87316508cafa69
Youssef Adnan <yadnan@connectmetv.com>
2016-05-04 15:00:21 +0200

    Upstream sync

--- osx/conf.py
+++ osx/conf.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 __package__ = "meld"
-__version__ = "3.15.4"
+__version__ = "3.16.0"
 
 # START; these paths are clobbered on install by meld.build_helpers
 DATADIR = os.path.join(sys.prefix, "share", "meld")
