00b49a77090b71fc6424ec6433a4c00a0b2da546
Kai Willadsen <kai.willadsen@gmail.com>
2017-04-07 06:18:11 +1000

    Revert "diffmap: Call the wrong function, for GTK+'s insanity"
    This reverts commit ef3ba1b87d2cb775901ac9780d464b32e5b3942c.
    
    The commit was an inadvertent requirement bump to GTK+ 3.22, and doesn't
    fix the problem properly anyway.

--- meld/diffmap.py
+++ meld/diffmap.py
@@ -108,7 +108,7 @@ class DiffMap(Gtk.DrawingArea):
         self._scroll_height = allocation.height
         self._width = max(allocation.width, 10)
         self._cached_map = None
-        self.queue_allocate()
+        self.queue_resize()
 
     def do_draw(self, context):
         if not self._setup:
