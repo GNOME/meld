64ffcc27cdd7bbc338eb23f7be9009ae809655a8
Kai Willadsen <kai.willadsen@gmail.com>
2017-12-20 07:08:08 +1000

    ui.gladesupport: Note why file exists, and "fix" flake8 errors

--- meld/ui/gladesupport.py
+++ meld/ui/gladesupport.py
@@ -1,11 +1,15 @@
 
-from meld import diffgrid
-from meld import diffmap
-from meld import linkmap
-from meld import preferences
-from meld import sourceview
-from meld.ui import emblemcellrenderer
-from meld.ui import historyentry
-from meld.ui import msgarea
-from meld.ui import notebook
-from meld.ui import statusbar
+# This file exists so that it can be referenced as an external Glade
+# module, allowing editing of our UI files without having widgets that
+# can't be instantiated everywhere.
+
+from meld import diffgrid  # noqa: F401
+from meld import diffmap  # noqa: F401
+from meld import linkmap  # noqa: F401
+from meld import preferences  # noqa: F401
+from meld import sourceview  # noqa: F401
+from meld.ui import emblemcellrenderer  # noqa: F401
+from meld.ui import historyentry  # noqa: F401
+from meld.ui import msgarea  # noqa: F401
+from meld.ui import notebook  # noqa: F401
+from meld.ui import statusbar  # noqa: F401
