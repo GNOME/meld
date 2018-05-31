GNOME/meld

commit 30bd3231bdbd6e23d69b6b2cc242a5f3fd29cdea

Add some support for numpad shortcuts (#175)

data/ui/filediff.ui
@@ -132,6 +132,7 @@
         <signal name="activate" handler="delete_change" swapped="no"/>
       </object>
       <accelerator key="Delete" modifiers="GDK_MOD1_MASK"/>
+      <accelerator key="KP_Delete" modifiers="GDK_MOD1_MASK"/>
     </child>
     <child>
       <object class="GtkAction" id="MergeFromLeft">

meld/meldwindow.py
@@ -189,6 +189,10 @@ class MeldWindow(Component):
         accels.connect(keyval, mask, 0, self.on_menu_edit_down_activate)
         (keyval, mask) = Gtk.accelerator_parse("<Primary>E")
         accels.connect(keyval, mask, 0, self.on_menu_edit_up_activate)
+        (keyval, mask) = Gtk.accelerator_parse("<Alt>KP_Down")
+        accels.connect(keyval, mask, 0, self.on_menu_edit_down_activate)
+        (keyval, mask) = Gtk.accelerator_parse("<Alt>KP_Up")
+        accels.connect(keyval, mask, 0, self.on_menu_edit_up_activate)
         (keyval, mask) = Gtk.accelerator_parse("F5")
         accels.connect(keyval, mask, 0, self.on_menu_refresh_activate)
 

commit 3604009b10868e684f0fe22bec8a4adaaea57da8

filediff: Fix change deletion shortcut for non-keypad deletes
This was broken by 30bd3231, because you can't actually expect multiple
accelerators to work.

The change here is kind of unpleasant. Really, it would be nice if
this was a generic helper that ran on focus, probably as part of MeldDoc
handling... but that's very overkill for this one accelerator.

data/ui/filediff.ui
@@ -132,7 +132,6 @@
         <signal name="activate" handler="delete_change" swapped="no"/>
       </object>
       <accelerator key="Delete" modifiers="GDK_MOD1_MASK"/>
-      <accelerator key="KP_Delete" modifiers="GDK_MOD1_MASK"/>
     </child>
     <child>
       <object class="GtkAction" id="MergeFromLeft">

meld/filediff.py
@@ -191,6 +191,11 @@ class FileDiff(MeldDoc, Component):
         self.actiongroup = self.FilediffActions
         self.actiongroup.set_translation_domain("meld")
 
+        # Alternate keybindings for a few commands.
+        self.extra_accels = (
+            ("<Alt>KP_Delete", self.delete_change),
+        )
+
         self.findbar = FindBar(self.grid)
         self.grid.attach(self.findbar.widget, 1, 2, 5, 1)
 
@@ -278,6 +283,22 @@ class FileDiff(MeldDoc, Component):
             t.line_renderer = renderer
 
         self.connect("notify::ignore-blank-lines", self.refresh_comparison)
+
+    def on_container_switch_in_event(self, ui):
+        MeldDoc.on_container_switch_in_event(self, ui)
+
+        accel_group = ui.get_accel_group()
+        for accel, callback in self.extra_accels:
+            keyval, mask = Gtk.accelerator_parse(accel)
+            accel_group.connect(keyval, mask, 0, callback)
+
+    def on_container_switch_out_event(self, ui):
+        accel_group = ui.get_accel_group()
+        for accel, callback in self.extra_accels:
+            keyval, mask = Gtk.accelerator_parse(accel)
+            accel_group.disconnect_key(keyval, mask)
+
+        MeldDoc.on_container_switch_out_event(self, ui)
 
     def get_keymask(self):
         return self._keymask