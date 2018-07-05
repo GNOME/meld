cd756be4014958055296823bd4b95df1dad5ba44
Vasily Galkin <galkin-vv@ya.ru>
2018-07-05 23:48:07 +0300

    freezed: remove XDG_DATA_DIRS hack
    recent win32 glib builds are confused with XDG_DATA_DIRS
    with ':' separator. Fortunately the hacks looks as not-needed anymore,
    since frozen meld meld is able to show icons and settings without this.

--- meld/conf.py
+++ meld/conf.py
@@ -33,12 +33,6 @@ def frozen():
     LOCALEDIR = os.path.join(melddir, "share", "mo")
     DATADIR_IS_UNINSTALLED = True
 
-    # This first bit should be unnecessary, but some things (GTK icon theme
-    # location, GSettings schema location) don't fall back correctly.
-    data_dir = os.environ.get('XDG_DATA_DIRS', "/usr/local/share/:/usr/share/")
-    data_dir = ":".join((melddir, data_dir))
-    os.environ['XDG_DATA_DIRS'] = data_dir
-
 
 def uninstalled():
     global DATADIR, LOCALEDIR, DATADIR_IS_UNINSTALLED
