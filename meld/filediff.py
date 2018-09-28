GNOME/meld

commit b0ba6344d18b6111b999d007fefda505ba80d681

filediff: Move focus grabbing until our initial comparison completes

meld/filediff.py
@@ -1034,7 +1034,6 @@ class FileDiff(melddoc.MeldDoc, gnomeglade.Component):
             self.msgarea_mgr[pane].clear()
 
         self.recompute_label()
-        self.textview[len(files) >= 2].grab_focus()
 
         self.undosequence.clear()
         self.linediffer.clear()
@@ -1150,6 +1149,8 @@ class FileDiff(melddoc.MeldDoc, gnomeglade.Component):
             yield i
         for i in self._diff_files():
             yield i
+        focus_pane = 0 if self.num_panes < 3 else 1
+        self.textview[focus_pane].grab_focus()
 
     def set_meta(self, meta):
         self.meta = meta

commit 871d4e054cf09d836118e66cab2e26e12717cace

filediff: Fix initial focus pane for two-pane comparison
This was a simple bad logic change from when I was reworking the logic
around file loading. It's almost as though implicitly using Booleans as
array indices is a bad idea.

Regression was introduced in b0ba6344.

meld/filediff.py
@@ -1272,7 +1272,7 @@ class FileDiff(MeldDoc, Component):
             yield i
         for i in self._diff_files():
             yield i
-        focus_pane = 0 if self.num_panes < 3 else 1
+        focus_pane = 0 if self.num_panes < 2 else 1
         self.textview[focus_pane].grab_focus()
 
     def set_meta(self, meta):