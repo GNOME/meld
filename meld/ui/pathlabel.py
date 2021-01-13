b7638b6ba878572ff59815fe0938c17a273ac714
Kai Willadsen <kai.willadsen@gmail.com>
2021-01-14 07:07:54 +1000

    ui.pathlabel: Update copyright dates

--- meld/ui/pathlabel.py
+++ meld/ui/pathlabel.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Kai Willadsen <kai.willadsen@gmail.com>
+# Copyright (C) 2019-2021 Kai Willadsen <kai.willadsen@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
