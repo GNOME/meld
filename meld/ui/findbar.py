4776eceeb05ed0d0966f38225c2b9826469f4813
Jack <jack.vanier16@outlook.com>
2021-01-16 14:58:39 -0500

    Fix imports

--- meld/ui/findbar.py
+++ meld/ui/findbar.py
@@ -14,8 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GObject, Gtk, GtkSource
 from typing import ClassVar, Optional
+
+from gi.repository import GObject, Gtk, GtkSource
 
 
 @Gtk.Template(resource_path='/org/gnome/meld/ui/findbar.ui')
