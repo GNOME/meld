6b0502dabd06229795dd7d7cc3a638d57f5b7d15
Kai Willadsen <kai.willadsen@gmail.com>
2021-07-25 09:57:06 +1000

    Add per-application preference for GTK's dark theme support (#571)

--- meld/settings.py
+++ meld/settings.py
@@ -15,7 +15,7 @@
 
 import sys
 
-from gi.repository import Gio, GObject, GtkSource, Pango
+from gi.repository import Gio, GObject, Gtk, GtkSource, Pango
 
 import meld.conf
 import meld.filters
@@ -35,6 +35,7 @@ class MeldSettings(GObject.GObject):
         self.on_setting_changed(settings, 'filename-filters')
         self.on_setting_changed(settings, 'text-filters')
         self.on_setting_changed(settings, 'use-system-font')
+        self.on_setting_changed(settings, 'prefer-dark-theme')
         self.style_scheme = self._style_scheme_from_gsettings()
         settings.connect('changed', self.on_setting_changed)
 
@@ -50,6 +51,10 @@ class MeldSettings(GObject.GObject):
         elif key in ('use-system-font', 'custom-font'):
             self.font = self._current_font_from_gsetting()
             self.emit('changed', 'font')
+        elif key == 'prefer-dark-theme':
+            gtk_settings = Gtk.Settings.get_default()
+            prefer_dark = settings.get_boolean(key)
+            gtk_settings.props.gtk_application_prefer_dark_theme = prefer_dark
         elif key in ('style-scheme'):
             self.style_scheme = self._style_scheme_from_gsettings()
             self.emit('changed', 'style-scheme')
