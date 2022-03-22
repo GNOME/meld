4cc699b74428fec87e2f818dbfd39e0cc3341ed3
Helly Guo <buffoonguo@gmail.com>
2022-03-22 21:20:49 +0800

    [feat]add a new feature: switch the panes - only supports in the two panes mode - the left pane to right and the right one to left

--- meld/dirdiff.py
+++ meld/dirdiff.py
@@ -497,6 +497,7 @@ class DirDiff(Gtk.VBox, tree.TreeviewCommon, MeldDoc):
             ('folder-compare-marked', self.action_diff_marked),
             ('folder-copy-left', self.action_copy_left),
             ('folder-copy-right', self.action_copy_right),
+            ('folder-switch', self.action_switch),
             ('folder-delete', self.action_delete),
             ('folder-expand', self.action_folder_expand),
             ('next-change', self.action_next_change),
@@ -1327,6 +1328,7 @@ class DirDiff(Gtk.VBox, tree.TreeviewCommon, MeldDoc):
             self.set_action_enabled(
                 'folder-compare-marked',
                 self.marked is not None and self.marked.pane != pane)
+            self.set_action_enabled('folder-switch', self.num_panes == 2)
             self.set_action_enabled('folder-delete', is_valid)
             self.set_action_enabled('folder-copy-left', is_valid and pane > 0)
             self.set_action_enabled(
@@ -1578,6 +1580,12 @@ class DirDiff(Gtk.VBox, tree.TreeviewCommon, MeldDoc):
 
     def action_copy_right(self, *args):
         self.copy_selected(1)
+
+    def action_switch(self, *args):
+        folder_x = self.folders[0]
+        self.folders[0] = self.folders[1]
+        self.folders[1] = folder_x
+        self.refresh()
 
     def action_delete(self, *args):
         self.delete_selected()
