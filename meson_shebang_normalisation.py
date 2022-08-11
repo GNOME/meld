1a8c44ea48f2e6cbdc11c47f0c5754c47c343a0d
Kai Willadsen <kai.willadsen@gmail.com>
2022-08-12 07:16:29 +1000

    Add missed shebang-stripping script

---
+++ meson_shebang_normalisation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import pathlib
+import sys
+
+
+def main():
+    in_path = pathlib.Path(sys.argv[1])
+    out_path = pathlib.Path(sys.argv[2])
+    if not in_path.exists():
+        print(f"Couldn't find {in_path}")
+        sys.exit(1)
+
+    lines = in_path.read_text().splitlines(keepends=True)
+    lines[0] = "".join(lines[0].split("env "))
+    out_path.write_text("".join(lines))
+
+
+if __name__ == "__main__":
+    main()
