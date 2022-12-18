d349102001ab118267d2ab27d34b86d4c153c34f
Kai Willadsen <kai.willadsen@gmail.com>
2022-12-18 11:42:15 +1000

    Fix Meson byte-compilation support with DESTDIR (#709)

--- meson_post_install.py
+++ meson_post_install.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 from compileall import compile_dir
 from os import environ, path
@@ -18,5 +19,11 @@ if not environ.get('DESTDIR', ''):
 # Byte-compilation is enabled by passing the site-packages path to this script
 if len(sys.argv) > 1:
     print('Byte-compiling Python module...')
+    destdir = os.getenv("DESTDIR", "")
     python_source_install_path = sys.argv[1]
-    compile_dir(path.join(python_source_install_path, 'meld'), optimize=1)
+    if destdir:
+        # The install path here will be absolute, so we can't use join()
+        install_path = destdir + os.path.sep + python_source_install_path
+    else:
+        install_path = python_source_install_path
+    compile_dir(os.path.join(install_path, "meld"), optimize=1)
