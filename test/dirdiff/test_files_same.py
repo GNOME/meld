ab58b3acf90c5a903afb8c9bd269517ba1b712ea
Kai Willadsen <kai.willadsen@gmail.com>
2023-12-16 15:38:57 +1000

    Fix dirdiff test to not use working directory
    This uses the pytest fixture helper so that it uses a per-test tmp path.
    We don't currently clean these up, but in any sane system this is going
    to be on a tmpfs, and we're not actually using significant amounts of
    disk space anyway.

--- test/dirdiff/test_files_same.py
+++ test/dirdiff/test_files_same.py
@@ -3,14 +3,9 @@ from os import path
 
 import pytest
 
-from .fixture import make
-
-DiffResult = Enum('DiffResult', 'Same SameFiltered DodgySame DodgyDifferent Different FileError')
-
-
-@pytest.fixture
-def differnt_dirs():
-    make()
+DiffResult = Enum(
+    "DiffResult", "Same SameFiltered DodgySame DodgyDifferent Different FileError"
+)
 
 
 def abspath(*args):
@@ -19,60 +14,78 @@ def abspath(*args):
 
 
 cmp_args = {
-    'shallow-comparison': False,
-    'time-resolution': 10000000000,
-    'ignore_blank_lines': True,
-    'apply-text-filters': True
+    "shallow-comparison": False,
+    "time-resolution": 10000000000,
+    "ignore_blank_lines": True,
+    "apply-text-filters": True,
 }
 
 no_ignore_args = dict(cmp_args)
-no_ignore_args['ignore_blank_lines'] = False
-no_ignore_args['apply-text-filters'] = False
+no_ignore_args["ignore_blank_lines"] = False
+no_ignore_args["apply-text-filters"] = False
 
 dodgy_args = dict(cmp_args)
-dodgy_args['shallow-comparison'] = True
+dodgy_args["shallow-comparison"] = True
 
 
-@pytest.mark.parametrize('files, regexes, comparison_args, expected', [
-    # empty file list
-    ((), [], cmp_args, DiffResult.Same),
-    # dirs are same
-    (('diffs/a', 'diffs/b'), [], cmp_args, DiffResult.Same),
-    # dir and file are different
-    (('diffs/a', 'diffs/b/b.txt'), [], cmp_args, DiffResult.Different),
-    # shallow equal (time + size)
-    (('diffs/a/d/d.txt', 'diffs/b/d/d.1.txt'), [], dodgy_args, DiffResult.DodgySame),
-    # empty files (fastest equal, won't read files)
-    (('diffs/a/c/c.txt', 'diffs/b/c/c.txt'), [], cmp_args, DiffResult.Same),
-    # 4.1kb vs 4.1kb file (slow equal, read both until end)
-    (('diffs/a/d/d.txt', 'diffs/b/d/d.txt'), [], cmp_args, DiffResult.Same),
-    # 4.1kb vs 4.1kb file (fast different, first chunk diff)
-    (('diffs/a/d/d.txt', 'diffs/b/d/d.1.txt'), [], cmp_args, DiffResult.Different),
-    # 4.1kb vs 4.1kb file (slow different, read both until end)
-    (('diffs/a/d/d.txt', 'diffs/b/d/d.2.txt'), [], cmp_args, DiffResult.Different),
-    # empty vs 1b file (fast different, first chunk diff)
-    (('diffs/a/e/g/g.txt', 'diffs/b/e/g/g.txt'), [], cmp_args, DiffResult.Different),
-    # CRLF vs CRLF with trailing, ignoring blank lines
-    (('diffs/a/crlf.txt', 'diffs/a/crlftrailing.txt'), [], cmp_args, DiffResult.SameFiltered),
-    # CRLF vs CRLF with trailing, not ignoring blank lines
-    (('diffs/a/crlf.txt', 'diffs/a/crlftrailing.txt'), [], no_ignore_args, DiffResult.Different),
-    # LF vs LF with trailing, ignoring blank lines
-    (('diffs/b/lf.txt', 'diffs/b/lftrailing.txt'), [], cmp_args, DiffResult.SameFiltered),
-    # LF vs LF with trailing, not ignoring blank lines
-    (('diffs/b/lf.txt', 'diffs/b/lftrailing.txt'), [], no_ignore_args, DiffResult.Different),
-    # CRLF vs LF, ignoring blank lines
-    (('diffs/a/crlf.txt', 'diffs/b/lf.txt'), [], cmp_args, DiffResult.SameFiltered),
-    # CRLF vs LF, not ignoring blank lines
-    (('diffs/a/crlf.txt', 'diffs/b/lf.txt'), [], no_ignore_args, DiffResult.Different),
-    # CRLF with trailing vs LF with trailing, ignoring blank lines
-    (('diffs/a/crlftrailing.txt', 'diffs/b/lftrailing.txt'), [], cmp_args, DiffResult.SameFiltered),
-    # CRLF with trailing vs LF with trailing, not ignoring blank lines
-    (('diffs/a/crlftrailing.txt', 'diffs/b/lftrailing.txt'), [], no_ignore_args, DiffResult.Different),
-])
-def test_files_same(files, regexes, comparison_args, expected, differnt_dirs):
+@pytest.mark.parametrize(
+    "files, regexes, comparison_args, expected",
+    [
+        # empty file list
+        ((), [], cmp_args, DiffResult.Same),
+        # dirs are same
+        (("a", "b"), [], cmp_args, DiffResult.Same),
+        # dir and file are different
+        (("a", "b/b.txt"), [], cmp_args, DiffResult.Different),
+        # shallow equal (time + size)
+        (("a/d/d.txt", "b/d/d.1.txt"), [], dodgy_args, DiffResult.DodgySame),
+        # empty files (fastest equal, won't read files)
+        (("a/c/c.txt", "b/c/c.txt"), [], cmp_args, DiffResult.Same),
+        # 4.1kb vs 4.1kb file (slow equal, read both until end)
+        (("a/d/d.txt", "b/d/d.txt"), [], cmp_args, DiffResult.Same),
+        # 4.1kb vs 4.1kb file (fast different, first chunk diff)
+        (("a/d/d.txt", "b/d/d.1.txt"), [], cmp_args, DiffResult.Different),
+        # 4.1kb vs 4.1kb file (slow different, read both until end)
+        (("a/d/d.txt", "b/d/d.2.txt"), [], cmp_args, DiffResult.Different),
+        # empty vs 1b file (fast different, first chunk diff)
+        (("a/e/g/g.txt", "b/e/g/g.txt"), [], cmp_args, DiffResult.Different),
+        # CRLF vs CRLF with trailing, ignoring blank lines
+        (("a/crlf.txt", "a/crlftrailing.txt"), [], cmp_args, DiffResult.SameFiltered),
+        # CRLF vs CRLF with trailing, not ignoring blank lines
+        (
+            ("a/crlf.txt", "a/crlftrailing.txt"),
+            [],
+            no_ignore_args,
+            DiffResult.Different,
+        ),
+        # LF vs LF with trailing, ignoring blank lines
+        (("b/lf.txt", "b/lftrailing.txt"), [], cmp_args, DiffResult.SameFiltered),
+        # LF vs LF with trailing, not ignoring blank lines
+        (("b/lf.txt", "b/lftrailing.txt"), [], no_ignore_args, DiffResult.Different),
+        # CRLF vs LF, ignoring blank lines
+        (("a/crlf.txt", "b/lf.txt"), [], cmp_args, DiffResult.SameFiltered),
+        # CRLF vs LF, not ignoring blank lines
+        (("a/crlf.txt", "b/lf.txt"), [], no_ignore_args, DiffResult.Different),
+        # CRLF with trailing vs LF with trailing, ignoring blank lines
+        (
+            ("a/crlftrailing.txt", "b/lftrailing.txt"),
+            [],
+            cmp_args,
+            DiffResult.SameFiltered,
+        ),
+        # CRLF with trailing vs LF with trailing, not ignoring blank lines
+        (
+            ("a/crlftrailing.txt", "b/lftrailing.txt"),
+            [],
+            no_ignore_args,
+            DiffResult.Different,
+        ),
+    ],
+)
+def test_files_same(create_sample_dir, files, regexes, comparison_args, expected):
     from meld.dirdiff import _files_same
 
-    files_path = abspath(*files)
+    files_path = [create_sample_dir / f for f in files]
     result = _files_same(files_path, regexes, comparison_args)
     actual = DiffResult(result + 1)
     assert actual == expected
