43c02869d32dca0a9045ae935a319cb7ae84e25d
Kai Willadsen <kai.willadsen@gmail.com>
2025-06-22 12:54:09 +1000

    Update statusbar copyright string

--- meld/ui/statusbar.py
+++ meld/ui/statusbar.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2013 Kai Willadsen <kai.willadsen@gmail.com>
+# Copyright (C) 2012-2025 Kai Willadsen <kai.willadsen@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
