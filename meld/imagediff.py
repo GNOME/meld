2a97212bbc8e8ed4c762c18b4de20578701c0f2b
Kai Willadsen <kai.willadsen@gmail.com>
2026-09-06 12:47:03 +1000

    Minor refactor to avoid filter() use

--- meld/imagediff.py
+++ meld/imagediff.py
@@ -78,12 +78,14 @@ def file_is_image(gfile):
 def files_are_images(gfiles):
     """Check if all files in the list are images."""
 
+    check_files = [f for f in gfiles if f]
+
     # Special case: if all files are blank, use text diff
-    if all(gfile is None for gfile in gfiles):
+    if not check_files:
         return False
 
     # Check if any non-blank file is not an image
-    for gfile in filter(None, gfiles):
+    for gfile in check_files:
         if not file_is_image(gfile):
             return False
 
